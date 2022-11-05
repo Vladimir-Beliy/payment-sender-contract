ba94c760836811cee42b33102145c3b4353225ff
Vladimir-Beliy <vovaazick@gmail.com>
2022-11-05 21:00:15 +0200

    Update _verifyVoucher method

--- contracts/PaymentSender.sol
+++ contracts/PaymentSender.sol
@@ -19,15 +19,23 @@ contract PaymentSender is Ownable, Pausable, EIP712 {
 
     mapping(address => uint256) private _nonce;
 
-    constructor(address voucherSigner_, address paymentToken_) EIP712("Payment Sender", "1") {
+    constructor(address voucherSigner_, address paymentToken_)
+        EIP712("Payment Sender", "1")
+    {
         voucherSigner(voucherSigner_);
         paymentToken(paymentToken_);
     }
 
-    function release(uint256 amount, bytes calldata voucher) external whenNotPaused {
+    function release(uint256 amount, bytes calldata voucher)
+        external
+        whenNotPaused
+    {
         uint256 nonce_ = _nonce[msg.sender];
 
-        _verifyVoucher(nonce_, amount, voucher);
+        require(
+            _verifyVoucher(nonce_, amount, voucher),
+            "PaymentSender: voucher is invalid"
+        );
 
         _nonce[msg.sender] = nonce_ + 1;
 
@@ -45,41 +53,61 @@ contract PaymentSender is Ownable, Pausable, EIP712 {
     }
 
     function voucherSigner(address voucherSigner_) public onlyOwner {
-        require(voucherSigner_ != address(0), "PaymentSender: voucher signer is zero address");
-        require(voucherSigner_.code.length == 0, "PaymentSender: voucher signer is contract address");
+        require(
+            voucherSigner_ != address(0),
+            "PaymentSender: voucher signer is zero address"
+        );
+        require(
+            voucherSigner_.code.length == 0,
+            "PaymentSender: voucher signer is contract address"
+        );
 
         _voucherSigner = voucherSigner_;
     }
 
     function paymentToken(address paymentToken_) public onlyOwner {
-        require(paymentToken_ != address(0), "PaymentSender: payment token is zero address");
-        require(paymentToken_.code.length > 0, "PaymentSender: payment token isn't contract address");
+        require(
+            paymentToken_ != address(0),
+            "PaymentSender: payment token is zero address"
+        );
+        require(
+            paymentToken_.code.length > 0,
+            "PaymentSender: payment token isn't contract address"
+        );
 
         _paymentToken = IERC20(paymentToken_);
     }
 
-    function voucherSigner() external view returns(address) {
+    function voucherSigner() external view returns (address) {
         return _voucherSigner;
     }
 
-    function paymentToken() external view returns(IERC20) {
+    function paymentToken() external view returns (IERC20) {
         return _paymentToken;
     }
 
-    function nonce(address account) external view returns(uint256) {
+    function nonce(address account) external view returns (uint256) {
         return _nonce[account];
     }
 
-    function _verifyVoucher(uint256 nonce_, uint256 amount, bytes calldata voucher) private {
-        bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(
-            keccak256("PaymentVoucher(address payee,uint256 nonce,uint256 amount)"),
-            msg.sender,
-            nonce_,
-            amount
-        )));
+    function _verifyVoucher(
+        uint256 nonce_,
+        uint256 amount,
+        bytes calldata voucher
+    ) private view returns (bool) {
+        bytes32 digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    keccak256("PaymentVoucher(address payee,uint256 nonce,uint256 amount)"),
+                    msg.sender,
+                    nonce_,
+                    amount
+                )
+            )
+        );
 
         address signer = ECDSA.recover(digest, voucher);
 
-        require(signer == _voucherSigner, "PaymentSender: voucher is invalid");
+        return signer == _voucherSigner;
     }
 }
